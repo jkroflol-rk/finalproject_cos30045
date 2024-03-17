27f42633e5052631f3be4805fc87b0ad9b8ea893
Lê Xuân Nhật <54104903+rt-kc@users.noreply.github.com>
2024-03-17 23:50:00 +0700

    addmore

--- script/choropleth.js
+++ script/choropleth.js
@@ -1,31 +1,43 @@
-let w = 500;
-let h = 300;
+function init() {
+    let w = 1000;
+    let h = 700;
 
-// Create a new projection using the Mercator projection
-let projection = d3.geoMercator()
-    .center([145, -36.5])
-    .scale(2450)
-    .translate([w / 2, h / 2]);
+    // Create a new projection using the Mercator projection
+    let projection = d3.geoNaturalEarth1()
+        .center([0, 0])
+        .scale(w / 1.5 / Math.PI)
+        .translate([w / 2, h / 2]);
 
-// Create a new path using the projection
-let path = d3.geoPath()
-    .projection(projection);
+    // Create a new path using the projection
+    let path = d3.geoPath()
+        .projection(projection);
 
-// Create a new SVG element
-let svg = d3.select("#map")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h)
-    .attr("fill", "lightblue");
+    // Create a new SVG element
+    let svg = d3.select("#chart")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h)
+        .attr("fill", "lightblue");
 
-// Load the JSON file and draw the map
-d3.json("dataset/world.json").then(function (json) {
-    svg.selectAll("path")
-        .data(json.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .attr("fill", "lightblue")
-        .attr("stroke", "grey")
-        .attr("stroke-width", 1);
-});
+    // Load the JSON file and draw the map
+    d3.json("dataset/world2.json").then(function (json) {
+        svg.selectAll("path")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr("fill", "grey")
+            .attr("stroke", "white")
+            .attr("stroke-width", 1)
+            .on("mouseover", function (d) {
+                d3.select(this).attr("fill", "orange");
+                console.log(d.properties.name);
+            })
+            .on("mouseout", function (d) {
+                d3.select(this).attr("fill", "grey");
+            });
+        console.log(json.features);
+    });
+}
+
+window.onload = init;
